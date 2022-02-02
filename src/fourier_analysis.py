1c3004d8a3804212ecae2cf465806ca8113240ca
lennytubby <lennart@slusny.de>
2022-02-02 20:53:12 +0100

    update

--- src/fourier_analysis.py
+++ src/fourier_analysis.py
@@ -38,7 +38,7 @@ def bin_spectrum(spectrum,rate):
 
 
 #Audio_file = 'Adele - Set Fire to the Rain'
-plots = True
+plots = False
 channel = 1             # for stereo songs we have two channels, choose one.
 
 # Messing around - setting stuff to 0
@@ -47,30 +47,28 @@ cutoff_right = 0        # Hz
 threshold_cut = 0       # Setting all frequencies with a lower value than threshold_cut times max to 0
 padding_offset = 0      # pad the signal with 0, front and back
 
-def getSpectum(spotify_url,output,Song_name):
-    if not output[-1] =="/":
-        output += "/"
-    subprocess.run(["spotdl", "--output", output, "--output-format", "wav", "--path-template", "song.{ext}", spotify_url])
-    rate, data = wav.read(output + 'song.wav') # rate = sampling rate (typically 48000)
-    os.remove(output + 'song.wav')
+def getSpectum(path,Song_name):
 
+    rate, data = wav.read(path) # rate = sampling rate (typically 48000)
+    print('Song name: ',Song_name)
     print('data shape (rows x channels) :',data.shape)
     if(len(data.shape) > 1):
 
         print('rate :',rate)
 
-        # pad signal
-        data = np.insert(data,0,np.repeat(np.array([[0,0]]),padding_offset,axis=0),axis=0)
-        data = np.append(data,np.repeat(np.array([[0,0]]),padding_offset,axis=0),axis=0)
+        # # pad signal
+        # data = np.insert(data,0,np.repeat(np.array([[0,0]]),padding_offset,axis=0),axis=0)
+        # data = np.append(data,np.repeat(np.array([[0,0]]),padding_offset,axis=0),axis=0)
 
         k = np.arange(data.shape[0])
         T = data.shape[0]/rate  
         frqLabel = k/T                  # Set correct x labels
 
         fft_out = fft(data[:,channel])
-        # setting stuff to 0
-        fft_out[np.where(frqLabel >= cutoff_left)[0][0]:np.where(frqLabel >= cutoff_right)[0][0]] = 0
-        fft_out[np.abs(fft_out)<np.abs(fft_out).max()*threshold_cut] = 0
+        
+        # # setting stuff to 0
+        # fft_out[np.where(frqLabel >= cutoff_left)[0][0]:np.where(frqLabel >= cutoff_right)[0][0]] = 0
+        # fft_out[np.abs(fft_out)<np.abs(fft_out).max()*threshold_cut] = 0
 
         if (plots):
             show_duration = data.shape[0]
@@ -83,23 +81,28 @@ def getSpectum(spotify_url,output,Song_name):
             # #plt.savefig('resuts/Spectrum_'+Song_name)
             plt.show()
         
-        return [fft_out, rate]
+        # cut out redunded information from fourier transform, namely the second half
+        fft_out_cut = fft_out[:fft_out.size//2+1]
+        
+        return [fft_out_cut, rate]
     else:
 
         print('rate :',rate)
 
-        # pad signal
-        data = np.insert(data,0,np.repeat(np.array([0]),padding_offset,axis=0),axis=0)
-        data = np.append(data,np.repeat(np.array([0]),padding_offset,axis=0),axis=0)
+        # # pad signal
+        # data = np.insert(data,0,np.repeat(np.array([0]),padding_offset,axis=0),axis=0)
+        # data = np.append(data,np.repeat(np.array([0]),padding_offset,axis=0),axis=0)
 
         k = np.arange(data.shape[0])
         T = data.shape[0]/rate  
         frqLabel = k/T                  # Set correct x labels
 
         fft_out = fft(data)
-        # setting stuff to 0
-        fft_out[np.where(frqLabel >= cutoff_left)[0][0]:np.where(frqLabel >= cutoff_right)[0][0]] = 0
-        fft_out[np.abs(fft_out)<np.abs(fft_out).max()*threshold_cut] = 0
+        
+        # # setting stuff to 0
+        # fft_out[np.where(frqLabel >= cutoff_left)[0][0]:np.where(frqLabel >= cutoff_right)[0][0]] = 0
+        # fft_out[np.abs(fft_out)<np.abs(fft_out).max()*threshold_cut] = 0
+        
         if (plots):
             show_duration = data.shape[0]
             # plt.title("Waveform of "+Song_name)
@@ -111,18 +114,31 @@ def getSpectum(spotify_url,output,Song_name):
             # # plt.savefig('Spectrum_'+Song_name)
             # plt.show()
         
-        return [fft_out, rate]
+        # cut out redunded information from fourier transform, namely the second half
+        fft_out_cut = fft_out[:fft_out.size//2+1]
+        
+        return [fft_out_cut, rate]
 
 
-# save back as an wav file
-fft_out,rate =  getSpectum("https://open.spotify.com/track/4aWmUDTfIPGksMNLV2rQP2", "data/temp", "some Song")
-sound_back = np.abs(ifft(fft_out))
-wav.write('results/output_'+'working'+'.wav', rate, np.int16(sound_back* 32767 / sound_back.max()))
-if (plots):
-    show_duration = sound_back.shape[0]
-    plt.title("Back formation of audio file")
-    plt.plot(np.arange(show_duration),sound_back[0:show_duration])
-    plt.show()
+## if you want to create a sound file from the cut down version of the spectrum (fft_out_cut), you need to add the redundand second half first
+# fft_out_cut_rejoined = np.concatenate((fft_out_cut,np.conjugate(fft_out_cut[1:-1])[::-1]))
+# rate = 2000 # ? may need to adjust 
+# sound_back = np.real(ifft(fft_out_cut_rejoined))
+# wav.write('results/output_'+'rejoined'+'.wav', rate, np.int16(sound_back* 32767 / sound_back.max()))
 
+
+# # try out fft and save back as an wav file -- for uncut fft
+# fft_out,rate =  getSpectum("file_path", "some Song")
+# sound_back = np.real(ifft(fft_out))
+# wav.write('results/output_'+'working'+'.wav', rate, np.int16(sound_back* 32767 / sound_back.max()))
+# if (plots):
+#     show_duration = sound_back.shape[0]
+#     plt.title("Back formation of audio file")
+#     plt.plot(np.arange(show_duration),sound_back[0:show_duration])
+#     plt.show()
+
+
+
+## create binned spectrum -- not working  -- for uncut fft
 # spectrum, rate = getSpectum("https://open.spotify.com/track/4aWmUDTfIPGksMNLV2rQP2", "data/temp", "some Song")
 # bin_spectrum(spectrum,rate)
