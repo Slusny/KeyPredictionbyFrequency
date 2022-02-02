4beb465439f3d41238cd9e028bfae513de23b621
julianzimmerlin <zim.julian@gmail.com>
2022-02-02 23:02:11 +0100

    Create dataset with spectrum (absolute values) as columns

--- src/append_fft_to_Dataset.py
+++ src/append_fft_to_Dataset.py
@@ -1,34 +1,48 @@
 import pandas as pd
 import os
 import numpy as np
-from fourier_analysis import getSpectum
+from src.fourier_analysis import getSpectum
 from pathlib import Path
 import matplotlib.pyplot as plt
 
 target_folder = 'data/piano'
 piano_2000_path  = 'data/piano_2000'
-file_name = 'dataset_with_spectrum.csv'
+file_name = 'dataset_abs.csv'
 
-def add_spectrum(row):
-    head, tail = os.path.split(row[index])
+
+def add_spectrum(file_path_string):
+    head, tail = os.path.split(file_path_string)
     name = tail.split('.')[0]
     file_name = name + '.wav'
     file_path = Path.joinpath(Path.cwd(),piano_2000_path,file_name)#.parents[0]
     # file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..',piano_2000_path,file_name)
     if file_path.exists():
         spectrum, rate = getSpectum(file_path.as_posix(),name)
-        return spectrum.tolist()
+        return spectrum, rate
     else:
         print('path ',file_path.as_posix(),' does not exist')
-        return np.nan
+        return np.nan, None
+
 
 def create_new_dataset():
     df = pd.read_pickle(os.path.join(target_folder, "dataset.pkl"))
-    index = np.where(df.columns.to_numpy() == 'file_path')[0][0]
-    df['spectrum'] = df.apply(lambda r: tuple(r), axis=1).apply(add_spectrum)
+    spectrum_series = df['file_path'].apply(add_spectrum)
+    rate = spectrum_series[0][1]
+    length = len(spectrum_series[0][0])
+    spectrum_series = spectrum_series.apply(lambda x: x[0])
+    lengths = spectrum_series.apply(len)
+    print(f'Deleting {len(lengths[lengths != length])} transforms with wrong length (should be {length})...')
+    spectrum_series = spectrum_series[lengths == length]
+    frqLabel = np.linspace(0.0, rate / 2.0, length)
+    spectrum_df = pd.DataFrame.from_dict(dict(zip(spectrum_series.index, spectrum_series.values)), orient='index')
+    spectrum_df.columns = frqLabel
+    spectrum_df = spectrum_df.apply(np.abs)
+    joined_df = df.join(spectrum_df).reset_index(drop=True)
+    print(joined_df.info())
+    print(f'Saving dataframe to {os.path.join(target_folder, file_name)}')
+    joined_df.to_csv(os.path.join(target_folder, file_name))
 
-    df.to_csv(os.path.join(target_folder, file_name))
-    
+
 def test_new_dataset():
     df = pd.read_csv(os.path.join(target_folder, file_name))
     x = 'n'
@@ -45,7 +59,7 @@ def test_new_dataset():
         x = input()
 
 # to test a dataset    
-test_new_dataset()
+#test_new_dataset()
 
 ## to create a dataset
-#create_new_dataset
+create_new_dataset()
