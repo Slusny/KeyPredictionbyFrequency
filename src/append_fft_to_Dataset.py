54acab7673c5dc34252218711708b3a0d96c163e
Lennart Slusny <lennart@slusny.de>
2022-02-06 13:08:12 +0100

    two datasets

--- src/append_fft_to_Dataset.py
+++ src/append_fft_to_Dataset.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 target_folder = 'data/dl_more_piano_4000'
 piano_2000_path  = 'data/dl_more_piano_4000'
 file_name = 'dataset_abs.csv'
+file_name_complex = 'dataset_complex.pkl'
 
 
 def add_spectrum(file_path_string):
@@ -36,11 +37,13 @@ def create_new_dataset():
     frqLabel = np.linspace(0.0, rate / 2.0, length)
     spectrum_df = pd.DataFrame.from_dict(dict(zip(spectrum_series.index, spectrum_series.values)), orient='index')
     spectrum_df.columns = frqLabel
-    spectrum_df = spectrum_df.apply(np.abs)
-    joined_df = df.join(spectrum_df, how='inner').reset_index(drop=True)
+    spectrum_df_abs = spectrum_df.apply(np.abs)
+    joined_df = df.join(spectrum_df_abs, how='inner').reset_index(drop=True)
+    joined_df_complex = df.join(spectrum_df, how='inner').reset_index(drop=True)
     print(joined_df.info())
     print(f'Saving dataframe to {os.path.join(target_folder, file_name)}')
     joined_df.to_csv(os.path.join(target_folder, file_name))
+    joined_df_complex.to_pickle(os.path.join(target_folder, file_name_complex))
 
 
 def test_new_dataset():
